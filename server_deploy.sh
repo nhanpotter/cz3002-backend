80939b829af858ed56fab6c6190daac9ca99b7ca
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2021-03-10 15:17:22 +0800

    add deploy script

---
+++ server_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git pull
+source .venv/bin/activate
+pipenv install
+cd cz3002_backend/
+python3 manage.py test
+python3 manage.py migrate
+ps axf|grep "manage.py"|grep -v grep| awk '{print "kill -9 " $1}'|sh
+nohup ./manage.py runserver 0:80 > nohup.out 2>&1 &
+
+echo "Done deploy"
