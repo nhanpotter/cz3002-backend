b91d9e3e410b6b2657eafce66257b8e648b0194b
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2021-03-21 02:57:58 +0800

    update more columns to admin page

--- cz3002_backend/authentication/admin.py
+++ cz3002_backend/authentication/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'phone_number', 'birthday')
